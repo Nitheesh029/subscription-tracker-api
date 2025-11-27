3e0d2a68dd7e8cca6121332b91ace3bedf405b49
Nitheesh029 <qxnitheesh@gmail.com>
2025-11-27 22:20:18 +0530

    installing dotenv package and creating env.js and exporting environmental variable

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import express from "express";
-
+import { PORT } from "./config/env.js";
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Welcome to Subscription Tracker API");
 });
 
-app.listen(3000, () => {
-  console.log(`Server is running on http://localhost:${3000}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
+console.log("Environment file loaded:", process.env.NODE_ENV);
